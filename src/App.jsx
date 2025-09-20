900737fe885b9ca4153b96cd542d0babcd992124
Rajratnam02 <rajratnam2005@gmail.com>
2025-09-20 20:58:20 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen w-screen flex items-center justify-center bg-zinc-900 text-white'>
+        
+    </div>
+  )
+}
+
+export default App
