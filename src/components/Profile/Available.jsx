9a0d21f5f22986e795ac2f1789d40a4480c83700
Rajratnam02 <rajratnam2005@gmail.com>
2026-01-12 22:46:53 +0530

    New branch for New UI

--- src/components/Profile/Available.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Available = () => {
-  return (
-    <div className='inter-500 text-md  bg-green-500 w-[130px]  rounded-full px-3 py-1 gap-5 text-green-800  flex items-center justify-center  '>Available</div>
-  )
-}
-
-export default Available
--- src/components/Profile/NotAvailable.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const NotAvailable = () => {
-  return (
-        <div className='inter-500 text-md w-[130px] bg-red-500   rounded-full px-3 py-1 gap-5 text-red-800  flex items-center justify-center  '>Not Available</div>
-  )
-}
-
-export default NotAvailable
